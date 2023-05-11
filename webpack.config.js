94a02eab3fbd337c0c193e1087e9ee4b4c2c3abb
Max <zeetreex@gmail.com>
2023-05-11 20:45:12 +0300

    js webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,19 @@
-module.exports = {}
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "build"),
+        clean: true
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html")
+        }),
+        new webpack.ProgressPlugin()
+    ]
+}
