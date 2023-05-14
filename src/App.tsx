8dc97d61a6b86fa9e5e34920ad1a699484896acf
Max <zeetreex@gmail.com>
2023-05-14 21:12:54 +0300

    global css variables, themes, custom hook for themes, theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, {Suspense} from 'react';
-import "./index.scss"
+import "./styles/index.scss"
 import {Link, Route, Routes} from "react-router-dom";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
       <Suspense fallback="Loading">
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import {render} from "react-dom";
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
+import ThemeProdiver from "./theme/ThemeProdiver";
 
 render(
   <BrowserRouter>
-    <App/>
+    <ThemeProdiver>
+      <App/>
+    </ThemeProdiver>
   </BrowserRouter>,
   document.getElementById("root")
 )
