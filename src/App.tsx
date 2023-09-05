6d31eb2608a25947412bd19d9e2adf799b30513f
Max <zeetreex@gmail.com>
2023-09-05 19:44:53 +0300

    global css variables/styles, custom hook for changing theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import "./index.scss";
-import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
+import "./styles/index.scss";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
       <Suspense fallback="loading...">
@@ -15,6 +18,7 @@ const App = () => {
           <Route path="/" element={<MainPageAsync />} />
         </Routes>
       </Suspense>
+      <button onClick={toggleTheme}>toggle theme</button>
     </div>
   );
 };
