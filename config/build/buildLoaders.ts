0390d0c3f88de5f8eb93e19428f2de46dfd3e2df
Max <zeetreex@gmail.com>
2023-09-10 20:56:56 +0300

    svg/file loader, custom button component

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,8 +1,14 @@
 import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import MiniCssExtractPlugin
+  from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
 
 export const buildLoaders = ({isDev}: BuildOptions): webpack.RuleSetRule[] => {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"]
+  };
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -28,7 +34,18 @@ export const buildLoaders = ({isDev}: BuildOptions): webpack.RuleSetRule[] => {
     exclude: /node_modules/,
   };
 
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff|woff2)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
+
   return [
+    fileLoader,
+    svgLoader,
     cssLoader,
     typescriptLoader
   ];
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,12 +5,11 @@ import "./styles/index.scss";
 import { Navbar } from "widgets/Navbar";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classNames("app", { hovered: true }, [theme])}>
       <Navbar />
-      <button onClick={toggleTheme}>toggle theme</button>
       <AppRouter />
     </div>
   );
