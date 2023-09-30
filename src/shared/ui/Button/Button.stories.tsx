22511b78f05541779bfb57c1286ca124433a572c
Max <zeetreex@gmail.com>
2023-09-30 21:46:33 +0300

    sidebar story, svg storybook loader

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,9 +1,7 @@
-import { ComponentStory, ComponentMeta, addDecorator } from "@storybook/react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Theme } from "app/providers/ThemeProvider";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Button, ThemeButton } from "./Button";
-
-import "../../../app/styles/index.scss";
 
 export default {
   title: "shared/Button",
